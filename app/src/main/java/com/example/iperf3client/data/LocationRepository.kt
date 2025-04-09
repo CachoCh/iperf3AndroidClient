c3bd86193e0ed96ec9a62e51c09ee9227c7900e0
casimiro.ch <email>
2025-04-09 21:16:30 +0200

    change: get location via LocationManager

--- app/src/main/java/com/example/iperf3client/data/LocationRepository.kt
+++ app/src/main/java/com/example/iperf3client/data/LocationRepository.kt
@@ -2,33 +2,54 @@ package com.example.iperf3client.data
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Context.LOCATION_SERVICE
 import android.location.Location
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.Priority
+import android.location.LocationManager
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class LocationRepository (context : Context) {
-    private var fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+    private var locationManager = context.getSystemService(LOCATION_SERVICE) as LocationManager
     private val _locationFlow = MutableStateFlow( Location(""))
 
     var locationFlow: StateFlow<Location> = _locationFlow.asStateFlow()
 
-
     @SuppressLint("MissingPermission")
     suspend fun getCurrentLocation() {
+        val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        val isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
 
-        fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
-            .addOnSuccessListener { location: Location? ->
-                if (location != null) {
-                    _locationFlow.value = location
-                }
-            }
+        var location: Location? = null
 
+        if (isGpsEnabled) {
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        }
 
+        if (location == null && isNetworkEnabled) {
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+        }
+
+        if (location != null) {
+            _locationFlow.value = location
+        }
     }
+/*
+    @SuppressLint("MissingPermission")
+    fun startLocationUpdates() {
+        val listener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                //Log.d("Location", "Updated Lat: ${location.latitude}, Lon: ${location.longitude}")
+            }
+        }
+
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            2000L, // minimum time interval (ms)
+            5f,    // minimum distance (meters)
+            listener
+        )
+    }*/
 
     companion object {
         @Volatile private var instance: LocationRepository? = null
